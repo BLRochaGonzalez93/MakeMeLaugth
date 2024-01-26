95b029966d559d8c067f2643f99b0d02cfdb7e38
Verónica Moreno <veronicamorenoalgarin@outlook.es>
2024-01-26 20:16:05 +0100

    2024.01.26.20.15

---
+++ MakeMeLaugth/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+       
+
+    public void SelectStart()
+    {
+        SceneManager.LoadScene("LevelChose");
+    }
+
+    public void SelectExit()
+    {
+        Application.Quit();
+
+    }
+    public void SelectCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
