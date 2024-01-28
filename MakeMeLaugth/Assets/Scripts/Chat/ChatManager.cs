a3c5609fbb6490d307c81a41b9ea50a8de02583d
BLRochaGonzalez93 <99007329+BLRochaGonzalez93@users.noreply.github.com>
2024-01-28 03:48:25 +0100

    2024.01.28.03.46
    + [SCRIPTABLE] Pricky Chat Messages
    ~ [SCRIPT] Modified ChatManager

--- MakeMeLaugth/Assets/Scripts/Chat/ChatManager.cs
+++ MakeMeLaugth/Assets/Scripts/Chat/ChatManager.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,21 +11,88 @@ public class ChatManager : MonoBehaviour
 {
     public CharacterChatBehaviour girl;
 
-    public List<TextMeshProUGUI> question;
+    public List<string> questions;
+    public List<Image> chatBlob;
     public List<TextMeshProUGUI> responses;
     private ChatBehaviour chat;
 
-    // Start is called before the first frame update
     void Start()
     {
         //girl.characterId = PlayerPrefs.GetInt("id");
         chat = girl.conversations[0];
-        //question.text = chat.question;
 
-        Debug.Log(responses.Count);
+        CargeQuestions();
+        ChargeResponses();
+        
+        StartCoroutine(ReplaceTexts());
+    }
+
+    private void CargeQuestions()
+    {
+        for (int i = 0; i < questions.Count; i++)
+        {
+            questions.Clear();
+        }
+
+        for (int i = 0; i < chat.questionInteraction.Count; i++)
+        {
+            questions.Add(chat.questionInteraction[i]);
+        }
+    }
+
+    private void ChargeResponses()
+    {
         for (int i = 0; i < responses.Count; i++)
         {
             responses[i].gameObject.transform.parent.gameObject.SetActive(false);
+        }
+    }
+
+    IEnumerator ReplaceTexts()
+    {
+        for (int i = 0; i < chatBlob.Count; i++)
+        {
+            chatBlob[i].gameObject.SetActive(false);
+            chatBlob[i].gameObject.GetComponentInChildren<TextMeshProUGUI>().text = "";
+        }
+
+        yield return new WaitForSeconds(1f);
+        if (questions.Count <= chatBlob.Count)
+        {
+            for (int i = 0; i < questions.Count; i++)
+            {
+                if (chatBlob[i].gameObject.activeSelf == false)
+                {
+                    chatBlob[i].gameObject.SetActive(true);
+                    chatBlob[i].gameObject.GetComponentInChildren<TextMeshProUGUI>().text = questions[i];
+                }
+                yield return new WaitForSeconds(1f);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < chatBlob.Count; i++)
+            {
+                if (chatBlob[i].gameObject.activeSelf == false)
+                {
+                    chatBlob[i].gameObject.SetActive(true);
+                    chatBlob[i].gameObject.GetComponentInChildren<TextMeshProUGUI>().text = questions[i];
+                }
+                yield return new WaitForSeconds(1f);
+            }
+
+            for (int i = chatBlob.Count; i < questions.Count; i++)
+            {
+
+                for (int j = 1; j < chatBlob.Count; j++)
+                {
+                    chatBlob[j-1].gameObject.GetComponentInChildren<TextMeshProUGUI>().text = chatBlob[j].gameObject.GetComponentInChildren<TextMeshProUGUI>().text;
+                }
+
+                chatBlob[chatBlob.Count-1].gameObject.GetComponentInChildren<TextMeshProUGUI>().text = questions[i];
+                
+                yield return new WaitForSeconds(1f);
+            }
         }
 
         for (int i = 0; i < chat.response.Count; i++)
@@ -35,97 +104,65 @@ public class ChatManager : MonoBehaviour
 
     public void ChargeNextR0()
     {
-        Debug.Log(chat.nextId[0]);
         if (chat.nextId[0] == 404) SceneManager.LoadScene(4);
         else if (chat.nextId[0] == 100) SceneManager.LoadScene(3);
         else
         {
             ChatBehaviour chat2 = girl.conversations[chat.nextId[0]];
             chat = chat2;
-            //question.text = chat2.question;
 
-            for (int i = 0; i < responses.Count; i++)
-            {
-                responses[i].gameObject.transform.parent.gameObject.SetActive(false);
-            }
+            CargeQuestions();
+            ChargeResponses();
 
-            for (int i = 0; i < chat2.response.Count; i++)
-            {
-                responses[i].text = chat2.response[i];
-                responses[i].gameObject.transform.parent.gameObject.SetActive(true);
-            }
+            StartCoroutine(ReplaceTexts());
         }
     }
 
     public void ChargeNextR1()
     {
-        Debug.Log(chat.nextId[1]);
         if (chat.nextId[1] == 404) SceneManager.LoadScene(4);
         else if (chat.nextId[1] == 100) SceneManager.LoadScene(3);
         else
         {
             ChatBehaviour chat2 = girl.conversations[chat.nextId[1]];
             chat = chat2;
-            //question.text = chat2.question;
 
-            for (int i = 0; i < responses.Count; i++)
-            {
-                responses[i].gameObject.transform.parent.gameObject.SetActive(false);
-            }
+            CargeQuestions();
+            ChargeResponses();
 
-            for (int i = 0; i < chat2.response.Count; i++)
-            {
-                responses[i].text = chat2.response[i];
-                responses[i].gameObject.transform.parent.gameObject.SetActive(true);
-            }
+            StartCoroutine(ReplaceTexts());
         }
     }
 
     public void ChargeNextR2()
     {
-        Debug.Log(chat.nextId[2]);
         if (chat.nextId[2] == 404) SceneManager.LoadScene(4);
         else if (chat.nextId[2] == 100) SceneManager.LoadScene(3);
         else
         {
             ChatBehaviour chat2 = girl.conversations[chat.nextId[2]];
             chat = chat2;
-            //question.text = chat2.question;
 
-            for (int i = 0; i < responses.Count; i++)
-            {
-                responses[i].gameObject.transform.parent.gameObject.SetActive(false);
-            }
+            CargeQuestions();
+            ChargeResponses();
 
-            for (int i = 0; i < chat2.response.Count; i++)
-            {
-                responses[i].text = chat2.response[i];
-                responses[i].gameObject.transform.parent.gameObject.SetActive(true);
-            }
+            StartCoroutine(ReplaceTexts());
         }
     }
 
     public void ChargeNextR3()
     {
-        Debug.Log(chat.nextId[3]);
         if (chat.nextId[3] == 404) SceneManager.LoadScene(4);
         else if (chat.nextId[3] == 100) SceneManager.LoadScene(3);
         else
         {
             ChatBehaviour chat2 = girl.conversations[chat.nextId[3]];
             chat = chat2;
-            //question.text = chat2.question;
 
-            for (int i = 0; i < responses.Count; i++)
-            {
-                responses[i].gameObject.transform.parent.gameObject.SetActive(false);
-            }
+            CargeQuestions();
+            ChargeResponses();
 
-            for (int i = 0; i < chat2.response.Count; i++)
-            {
-                responses[i].text = chat2.response[i];
-                responses[i].gameObject.transform.parent.gameObject.SetActive(true);
-            }
+            StartCoroutine(ReplaceTexts());
         }
     }
 }
